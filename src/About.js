d94c7b68f87ae3ac5604828e50227f56a212a6fb
AnaR <125403754+cushkorea2@users.noreply.github.com>
2023-10-03 11:22:09 -0400

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn about becoming an expat in different countries.</p>
+    </div>
+  );
+};
+
+export default About;
